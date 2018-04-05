3e52978c282b67fbe409b0ddb40dd7b414e8e825
Rupesh Acharya <acharya.ru@husky.neu.edu>
2018-04-05 02:55:55 -0400

    Adding Basic Login Page

--- announcementsFrontend/src/app/app.module.ts
+++ announcementsFrontend/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {FormsModule} from "@angular/forms";
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import {MatIconModule} from "@angular/material";
 import { AppComponent } from './app.component';
 import { SingupComponent } from './userservices/singup/singup.component';
+import { LoginComponent } from './userservices/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingupComponent
+    SingupComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
