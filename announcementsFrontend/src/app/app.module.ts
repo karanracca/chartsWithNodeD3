e6c0b0d25b754eff5b78ab7bf2114e644ae1a701
Niyati <maheshwari.ni@husky.neu.edu>
2018-04-09 16:05:52 -0400

    Adding the routing between login and signup page

--- announcementsFrontend/src/app/app.module.ts
+++ announcementsFrontend/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
-import {HttpClientModule} from '@angular/common/http';
+
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './userservices/login/login.component';
