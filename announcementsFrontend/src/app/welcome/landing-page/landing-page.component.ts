fdb8c54d043ad48b9e1b8a81cec3063b4ac4592e
Karan Racca <racca.k@husky.neu.edu>
2018-04-12 13:17:08 -0400

    Merging with working karan

---
+++ announcementsFrontend/src/app/welcome/landing-page/landing-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CreateChartComponent } from '../../charts/create-chart/create-chart.component';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
