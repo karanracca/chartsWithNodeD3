384fdb8903d01a32e54c38fff019f897ad452ebe
Niyati <maheshwari.ni@husky.neu.edu>
2018-04-16 17:47:06 -0400

    Adding code for line chart in html and ts file

--- announcementsFrontend/src/app/charts/line-chart/line-chart.component.ts
+++ announcementsFrontend/src/app/charts/line-chart/line-chart.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {PapaParseService} from "ngx-papaparse";
+import {ChartService} from "../charts.service";
 
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
-  styleUrls: ['./line-chart.component.scss']
+  styleUrls: ['./line-chart.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class LineChartComponent implements OnInit {
 
-  constructor() { }
+  lineChartForm: FormGroup;
+  fileData: any;
+  file: File;
+  showDataFields = false;
+  html: any;
+
+  constructor(private papa: PapaParseService, private chartService: ChartService) { }
 
   ngOnInit() {
+    this.lineChartForm = new FormGroup({
+      xaxis: new FormControl('', [<any>Validators.required]),
+      yaxis: new FormControl('', [<any>Validators.required]),
+    });
+  }
+
+  onSubmit(selectedValues , isValid: boolean) {
+    if (isValid && selectedValues.xaxis !== selectedValues.yaxis) {
+      console.log(selectedValues);
+      this.chartService.createLineChart(selectedValues, this.file).subscribe((data) => {
+        console.log(data);
+        this.html = `data:image/png;base64,${data.payload}`;
+      });
+    }
+  }
+
+  getData (file) {
+    return new Promise((resolve, reject) => {
+      this.papa.parse(file, {
+        header: true,
+        error: (error) => reject(error),
+        complete: (data) => resolve(data)
+      });
+    });
+  }
+
+  onFileSelected(file) {
+    this.file = file;
+    this.getData(file).then(data => {
+      this.fileData = data;
+      console.log('File Data', this.fileData);
+      this.showDataFields = true;
+    });
   }
 
 }
