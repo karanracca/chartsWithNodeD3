0ca5e3c766dd8c1920af52d2ceb724741c78e21e
Karan Racca <racca.k@husky.neu.edu>
2018-04-24 13:26:25 -0400

    Adding styles for announcemets

---
+++ announcements-angular/src/app/userservices/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {UserServices} from '../user.service';
+import {Router} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {NotificationService} from '../../shared/notification.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(private userService: UserServices,
+              private router: Router,
+              private notifyService: NotificationService) { }
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [<any>Validators.required]),
+      password: new FormControl('', [<any>Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  getPasswordError() {
+    return this.loginForm.controls.password.hasError('required') ? 'Password is required' :
+      this.loginForm.controls.password.hasError('minlength') ? 'Password should be minimum 8 characters long' : '';
+  }
+
+  login(credentials: {username, password}, isValid: boolean) {
+    if (isValid) {
+      this.userService.login(credentials.username, credentials.password).subscribe((data: any) => {
+        this.notifyService.notification.next(data.message);
+        this.router.navigate(['/welcome']);
+      }, error => {
+        this.notifyService.notification.next(error);
+      });
+    }
+  }
+}
