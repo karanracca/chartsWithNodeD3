deeee608b0cab4b6b95760b7ba19c78ce2c7c1b5
Niyati <maheshwari.ni@husky.neu.edu>
2018-04-24 16:42:52 -0400

    Adding spinner for api calls

--- announcements-node/controller/user.controller.js
+++ announcements-node/controller/user.controller.js
@@ -2,10 +2,8 @@ const {USER_ROLE, DBNAME, SECRET, USER_COLLECTION} = require('../shared/app-cons
 const DBService = require('../shared/db.service');
 const jwt = require('jsonwebtoken');
 const ObjectID = require('mongodb').ObjectID;
-<<<<<<< HEAD
 const userService = require('../service/user.service');
 
-=======
 var nodemailer = require('nodemailer');
 var accountSid = 'ACbdc6403769edfc193cc8cc9799def491';
 var authToken = '1b514df52af8fbcb2dfd85bc05114c54';
@@ -13,7 +11,6 @@ const client = require('twilio')(accountSid, authToken);
 const generator = require('generate-password');
 
 //Function to create a new user
->>>>>>> f9604679fdf8ae06219ff37dbd9af1d3e4908882
 exports.createUser = function (req, res) {
 
     DBService.findOne({$or: [{username: req.body.username}, {email: req.body.email}]}, DBNAME, 'users').then(function (userObject) {
@@ -44,14 +41,6 @@ exports.createUser = function (req, res) {
 
             DBService.insertOne(userInfo, DBNAME, USER_COLLECTION).then(function () {
 
-<<<<<<< HEAD
-                mailer.sendMail(mailer.createMailConfiguration(
-                    req.body.email,
-                    'Welcome to Charts',
-                    'Dear ' +req.body.firstName + ',\nThank you for registering with us you can now make charts using your credits.\n\nRegards,\nCharts Team'
-                ));
-
-=======
                 var mailOptions = {
                     from: 'acharya.rupesh0@gmail.com',
                     to: req.body.email,
@@ -73,7 +62,6 @@ exports.createUser = function (req, res) {
                     to: '+18573188747',
                     from: '+13396746626'
                 });
->>>>>>> f9604679fdf8ae06219ff37dbd9af1d3e4908882
                 res.status(200).json({
                     success: true,
                     message: `User ${userInfo.firstName} registered.`
@@ -139,7 +127,6 @@ exports.deleteUser = function (req, res) {
     });
 };
 
-<<<<<<< HEAD
 exports.resetPassword = async function (req, res) {
     if (req.body.email) {
         try {
@@ -155,41 +142,6 @@ exports.resetPassword = async function (req, res) {
             res.status(400).send({
                 success: false,
                 message: error.message
-=======
-//Function to reset the password
-exports.resetPassword = function (req, res) {
-    DBService.findOne({email: req.body.emailFormControl}, DBNAME, 'users').then(function (userObject) {
-        if (userObject.email === req.body.emailFormControl) {
-
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: 'youremail@gmail.com',
-                    pass: 'yourpassword'
-                }
-            });
-
-
-            var newPassword = generator.generate({
-                length: 10,
-                numbers: true
-            });
-
-            var mailOptions = {
-                from: 'youremail@gmail.com',
-                to: req.body.emailFormControl,
-                subject: 'Reset Password Mail',
-                text: 'Your new password is ' + newPassword
-            };
-
-            transporter.sendMail(mailOptions, function (error, info) {
-                console.log(mailOptions);
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
->>>>>>> f9604679fdf8ae06219ff37dbd9af1d3e4908882
             });
         }
     } else {
@@ -202,8 +154,6 @@ exports.resetPassword = function (req, res) {
 
 exports.updateUser = async function (req, res) {
     if (req.params.id) {
-
-<<<<<<< HEAD
         try {
             let result = await userService.updateUser(req.body, req.params.id);
             console.log('final', result);
@@ -216,15 +166,6 @@ exports.updateUser = async function (req, res) {
             }
         } catch (error) {
             res.status(400).send({
-=======
-            res.status(200).send({
-                success: true,
-                message: 'A temporary password has been sent to your registered email Id'
-            })
-        } else {
-            console.log("Hiii");
-            return res.status(500).send({
->>>>>>> f9604679fdf8ae06219ff37dbd9af1d3e4908882
                 success: false,
                 message: error.message
             });
@@ -237,7 +178,6 @@ exports.updateUser = async function (req, res) {
     }
 };
 
-<<<<<<< HEAD
 exports.getCredits = async function (req, res) {
     let credits = await userService.getCredits(req.header('x-access-token'));
     console.log("Credits", credits);
@@ -248,27 +188,4 @@ exports.getCredits = async function (req, res) {
             message: 'Credits retrived'
         })
     }
-=======
-//Function to update user
-exports.updateUser = function (req, res) {
-
-    DBService.findOne({$or: [{username: req.body.username}, {email: req.body.email}]}, DBNAME, 'users').then(function (userObject) {
-        let userInfo = {
-            $set: {
-                username: req.body.username,
-                password: req.body.password,
-                firstName: req.body.firstname,
-                lastName: req.body.lastname,
-                email: req.body.email,
-                phone: req.body.phone,
-                role: USER_ROLE,
-                credits: 10
-            }
-        };
-
-        DBService.updateOne(userInfo, DBNAME).then(function () {
-            console.log('User updated Successfully');
-        })
-    });
->>>>>>> f9604679fdf8ae06219ff37dbd9af1d3e4908882
 };
